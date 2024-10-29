bedeafd67fb01a5c02181e3368cdda0eb3936e64
Eldar Dikopp <eldar.dikopp@yandex.ru>
2024-10-29 20:13:41 +0300

    added tests pass rec

--- pages/recovery_page.py
+++ pages/recovery_page.py
@@ -2,6 +2,7 @@ import allure
 from pages.base_page import BasePage
 from src.data import *
 from src.locators import Locators
+from src.helpers import random_reg_email
 
 
 class RecoveryPass(BasePage):
@@ -9,7 +10,7 @@ class RecoveryPass(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    @allure.step('Открываем сайт')
+    @allure.step('Открываем сайт "Login"')
     def login_open(self):
         self.open(URL_LOGIN)
 
@@ -24,3 +25,27 @@ class RecoveryPass(BasePage):
     @allure.step('получение сайта «Восстановить пароль»')
     def get_pass_rec_url(self):
         self.get_current_url()
+
+    @allure.step('Открываем сайт "Recovery"')
+    def rec_open(self):
+        self.open(URL_PASS_REC)
+
+    @allure.step('Вписывает email')
+    def send_email(self):
+        self.send_keys(Locators.CONT_PASS_REC_EMAIL, random_reg_email())
+
+    @allure.step('Вписывает email')
+    def press_rec_button(self):
+        self.click(Locators.BUTTON_PASS_REC)
+
+    @allure.step('Проверка наличия кнопки "Сохранить"')
+    def get_save_button(self):
+        return self.wait_for_element(Locators.BUTTON_PASS_SAVE)
+
+    @allure.step('Нажать на кнопку видимости пароля')
+    def press_vis_button(self):
+        self.click(Locators.BUTTON_PASS_VIS)
+
+    @allure.step('Вилимость активного окна пароля')
+    def get_active_cont(self):
+        return self.wait_for_element(Locators.CONT_PASS_ACTIVE)
--- src/locators.py
+++ src/locators.py
@@ -4,3 +4,8 @@ from selenium.webdriver.common.by import By
 class Locators:
     BUTTON_PASS_LINK = (By.XPATH, ".//div/p/a[@href= '/forgot-password']")
     TAG_PASS_REC = (By.XPATH, ".//button[text() = 'Восстановить']")
+    CONT_PASS_REC_EMAIL = (By.XPATH, ".//div[@class = 'input__container']//input")
+    BUTTON_PASS_REC = (By.XPATH, ".//button[text() = 'Восстановить']")
+    BUTTON_PASS_SAVE = (By.XPATH, ".//div/label[text() = 'Введите код из письма']")
+    BUTTON_PASS_VIS = (By.XPATH, ".//div[@class = 'input__icon input__icon-action']")
+    CONT_PASS_ACTIVE = (By.XPATH, ".//div/div[contains(@class, 'input_status_active')]")
