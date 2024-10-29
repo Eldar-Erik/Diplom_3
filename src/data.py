1d80a827ef88d1c9c1146234dab5cb2458574e18
Eldar Dikopp <eldar.dikopp@yandex.ru>
2024-10-30 00:55:00 +0300

    added personal account tests

--- src/data.py
+++ src/data.py
@@ -1,6 +1,8 @@
 URL_HOME = 'https://stellarburgers.nomoreparties.site/'
 URL_LOGIN = 'https://stellarburgers.nomoreparties.site/login'
 URL_PASS_REC = 'https://stellarburgers.nomoreparties.site/forgot-password'
+URL_PROFILE = 'https://stellarburgers.nomoreparties.site/account/profile'
+URL_ORDER_HISTORY = 'https://stellarburgers.nomoreparties.site/account/order-history'
 
 USERNAME = f'eldar_dikopp_10_012'
 LOG_EMAIL = f'{USERNAME}@yandex.ru'
--- tests/test_pers_account.py
+++ tests/test_pers_account.py
@@ -1,4 +1,33 @@
-'''Проверь:
--переход по клику на «Личный кабинет»,
--переход в раздел «История заказов»,
--выход из аккаунта.'''
+import allure
+from pages.account_page import AccountPage
+
+
+class TestAccountPage:
+
+    @allure.title('Переход по клику на «Личный кабинет»')
+    def test_personal_account_button_click_success(self, driver):
+        acc = AccountPage(driver)
+        acc.login_open()
+        acc.login_enter()
+        acc.account_button_click()
+        assert acc.wait_for_profile_site()
+
+    @allure.title('Переход в раздел «История заказов»')
+    def test_order_list_success(self, driver):
+        acc = AccountPage(driver)
+        acc.login_open()
+        acc.login_enter()
+        acc.account_button_click()
+        acc.wait_for_profile_site()
+        acc.history_button_click()
+        assert acc.wait_for_history_list()
+
+    @allure.title('Выход из аккаунта')
+    def test_personal_account_quit_button_click_success(self, driver):
+        acc = AccountPage(driver)
+        acc.login_open()
+        acc.login_enter()
+        acc.account_button_click()
+        acc.wait_for_profile_site()
+        acc.click_quit_button()
+        assert acc.get_login_page()
